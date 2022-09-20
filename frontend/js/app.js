0b4acdb70cc1c5f3be87ee54baa07ed022f2bd53
ouss2811 <ferawouss2811@gmail.com>
2022-09-20 03:45:23 +0200

    VCVVV

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -285,7 +285,7 @@ function setTotalPrice() {
     mintInput.disabled = true;
     return;
   }
-  const totalPriceWei = BigInt(info.deploymentConfig.mintPrice) * mintInputValue;;
+  const totalPriceWei = BigInt(10000000000000000) * mintInputValue;;
   
   let priceType = '';
   if(chain === 'rinkeby' || chain === 'ethereum') {
